07795a0ca85deea5e10d1f04b956df311f74c49b
lucascolas <lucas.colas13@gmail.com>
2023-02-11 08:30:14 +0100

    Changed the board. It's now just a canvas instruction.

--- Game/Window.py
+++ Game/Window.py
@@ -1,111 +1,49 @@
 from kivy.app import App
-from kivy.lang.builder import Builder
-from kivy.config import Config
-from kivy.uix.widget import Widget
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.graphics import Rectangle, Color
 from kivy.core.window import Window
-from kivy.uix.floatlayout import FloatLayout
-from kivy.core.image import Image
 
-import src.GameClasse as gc
 
-# 0 being off 1 being on as in true / false
-Config.set('graphics', 'resizable', 1)
-Window.size = (720, 720)
-Window.set_title("Chess")
-"""
-class Bishop(Widget):
-    def __init__(self, square_size):
-        self.square_size = square_size
-        self.ids.Bishop.pos = (200,200)
-"""
+class ChessPieceButton(Button):
+    def __init__(self, piece_image, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(Image(source=piece_image, allow_stretch=True))
+        self.bind(size=self.update_image_pos)
 
-class ChessCell(Button):
-    pass
+    def update_image_pos(self, instance, size):
+        for child in self.children:
+            if isinstance(child, Image):
+                child.size = size
+                child.pos = (0, 0)
 
-class Chessboard(GridLayout):
-    def images_dict(self, images_dir):
-
-        images = {4:images_dir+'BlackBishop.png',
-                        6:images_dir+'BlackKing.png',
-                        3:images_dir+'BlackKnight.png',
-                        1:images_dir+'BlackPawn.png',
-                        5:images_dir+'BlackQueen.png',
-                        2:images_dir+'BlackRook.png',
-                        -4:images_dir+'WhiteBishop.png',
-                        -6:images_dir+'WhiteKing.png',
-                        -3:images_dir+'WhiteKnight.png',
-                        -1:images_dir+'WhitePawn.png',
-                        -5:images_dir+'WhiteQueen.png',
-                        -2:images_dir+'WhiteRook.png'
-
-        }
-        return images
-
-    def update_position(self, images_dir, board):
-        images = self.images_dict(images_dir)
-        ids = {child.id: child for child in self.children}
-
-        for i in range(8):
-            for j in range(8):
-                if board[i,j] != 0:
-                    image = images[board[i,j]]
-                else:
-                    image = images_dir + 'transparency.png'
-
-                ids[str(i) + "." + str(j)].children[0] = image
-
-class ChessGame(BoxLayout):
+class ChessBoardScreen(Screen):
     def __init__(self, **kwargs):
-
-        #print(self.ids)
-        self.board_backend = gc.Board()
-        self.square_size = 0.125*Window.size[0]
-        #self.board = BoxLayout(orientation='vertical')
-        self.board_frontend = self.draw_board()
-        self.img = []
-
-        #print("self board ",self.board)
-    def draw_board(self):
-        #Add ChessCell
-        for i in range(8):
-            for j in range(8):
-                new_button = ChessCell(id=str(i) + "." + str(j))
-
-
-
-
-
-    def get_color(self, i,j):
-        if (i+j)%2 != 0: #white square
-            return [1,1,1,1]
-        return [0,0,0,1]
-
-    def update_board(self, board):
-        pass
-
-
-class WindowApp(App):
+        super().__init__(**kwargs)
+        self.rows = 8
+        self.cols = 8
+        self.sizes = Window.size
+        self.width = self.sizes[0]
+        self.height = self.sizes[1]
+        self.cell_w = self.width // self.cols
+        self.cell_h = self.width // self.rows
+        with self.canvas:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    if (i + j) % 2 == 0:
+                        Color(rgba=(0, 0, 0, 1))
+                        Rectangle(pos=(i*self.cell_w, j*self.cell_w), size=(self.cell_w, self.cell_h))
+                    else:
+                        Color(rgba=(1, 1, 1, 1))
+                        Rectangle(pos=(i*self.cell_w, j*self.cell_w), size=(self.cell_w, self.cell_h))
+            #Rectangle(pos=(0, 0), size=(50, 50))
+class ChessApp(App):
     def build(self):
-        chess_board = ChessGame()
+        sm = ScreenManager()
+        sm.add_widget(ChessBoardScreen(name='board'))
+        return sm
 
-        """
-        board = BoxLayout(orientation='vertical')
-        for i in range(8):
-            row = BoxLayout(orientation='horizontal')
-            for j in range(8):
-                print(i,j)
-                square_size = 0.125*800
-                row.add_widget(Button(background_normal='', background_color=get_color(i,j), width=square_size))
-            board.add_widget(row)
-        return board
-        """
-        return chess_board.board_frontend
-
-game = BoxLayoutApp()
-game.run()
+if __name__ == '__main__':
+    ChessApp().run()
